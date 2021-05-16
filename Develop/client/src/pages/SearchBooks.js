35f309d3c18a02fe43647ad3c1cee33f9420dd84
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-05-16 16:16:49 -0500

    added queries and mutation in the components and pages

--- Develop/client/src/pages/SearchBooks.js
+++ Develop/client/src/pages/SearchBooks.js
@@ -59,7 +59,7 @@ const SearchBooks = () => {
   // create function to handle saving a book to our database
   const handleSaveBook = async (bookId) => {
     // find the book in `searchedBooks` state by the matching id
-    const bookToSave = searchedBooks.find((book) => book.bookId === bookId);
+    const book = searchedBooks.find((book) => book.bookId === bookId);
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -71,11 +71,11 @@ const SearchBooks = () => {
     try {
       // execute the SAVE_BOOK mutation
       await saveBook({
-        variables: { bookToSave }
+        variables: { book }
       })
 
       // if book successfully saves to user's account, save book id to state
-      setSavedBookIds([...savedBookIds, bookToSave.bookId]);
+      setSavedBookIds([...savedBookIds, book.bookId]);
     } catch (err) {
       console.error(err);
     }
